d75f95463724e5503a0fd39f13d7e50e298e5d3d
hwigkim <kyh2351313@gmail.com>
2026-06-24 17:25:46 +0900

    debug: collect logs and write in after_write hook

--- blog/_plugins/wylie_converter.rb
+++ blog/_plugins/wylie_converter.rb
@@ -1,5 +1,7 @@
 require 'open3'
-require 'fileutils'
+
+# 글로벌 로그 수집 배열
+$wylie_logs = []
 
 Jekyll::Hooks.register [:documents, :pages], :pre_render do |doc|
   # 파일 경로가 없거나 마크다운 파일이 아니면 건너뜁니다.
@@ -7,18 +9,11 @@ Jekyll::Hooks.register [:documents, :pages], :pre_render do |doc|
   ext = File.extname(doc.path).downcase
   next unless ext == '.md' || ext == '.markdown'
   
-  # 디버그 로그 파일 경로 설정 (소스 폴더에 작성하여 빌드 시 _site로 복사되도록 함)
-  debug_file = File.join(doc.site.source, "wylie_debug.txt")
-  
-  File.open(debug_file, "a:utf-8") do |f|
-    f.puts "--- Processing: #{doc.path} ---"
-  end
+  $wylie_logs << "--- Processing: #{doc.path} ---"
   
   content = doc.content
   if content && content.include?('tibwc*')
-    File.open(debug_file, "a:utf-8") do |f|
-      f.puts "Found 'tibwc*' keyword in #{doc.path}"
-    end
+    $wylie_logs << "Found 'tibwc*' keyword in #{doc.path}"
 
     # 파이썬을 서브프로세스로 실행하여 Wylie 표기를 티베트 유니코드로 변환
     # PYTHONUTF8=1 환경변수를 설정하여 윈도우 인코딩 오류 방지
@@ -40,13 +35,11 @@ Jekyll::Hooks.register [:documents, :pages], :pre_render do |doc|
     stdout.force_encoding("UTF-8")
     stderr.force_encoding("UTF-8")
 
-    File.open(debug_file, "a:utf-8") do |f|
-      f.puts "Python subprocess success: #{status.success?}"
-      f.puts "Stdout length: #{stdout.length}"
-      f.puts "Stderr: #{stderr}"
-      if status.success?
-        f.puts "Sample output: #{stdout[0..300]}"
-      end
+    $wylie_logs << "Python subprocess success: #{status.success?}"
+    $wylie_logs << "Stdout length: #{stdout.length}"
+    $wylie_logs << "Stderr: #{stderr}"
+    if status.success?
+      $wylie_logs << "Sample output: #{stdout[0..300]}"
     end
 
     if status.success?
@@ -55,4 +48,10 @@ Jekyll::Hooks.register [:documents, :pages], :pre_render do |doc|
       raise "Wylie Converter [Error] on #{doc.path}: #{stderr}"
     end
   end
+end
+
+Jekyll::Hooks.register :site, :after_write do |site|
+  # 빌드가 완전히 완료된 후 _site 폴더에 직접 디버그 로그 파일 쓰기
+  debug_file = File.join(site.dest, "wylie_debug.txt")
+  File.write(debug_file, $wylie_logs.join("\n"), encoding: "utf-8")
 end
